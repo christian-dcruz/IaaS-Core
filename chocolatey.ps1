7198487f7e6b69ffa7d3ac404edac2f9ad4beeb1
Christian <a@b.com>
2018-11-29 21:55:22 +0000

    Added choco.ps1 invocation

--- chocolatey.ps1
+++ chocolatey.ps1
@@ -6,9 +6,4 @@ choco install -y nodejs
 choco install -y vscode
 choco install -y visualstudio2017community
 choco install -y gnuwin32-coreutils.install
-setx PATH "%PATH%;C:\Program Files (x86)\GnuWin32\bin"
 choco install -y curl
-mkdir C:\Repos
-pushd C:\Repos
-copy /y global.gitconfig c:\users\cdcadmin\.gitconfig
-git clone https://github.com/christian-dcruz/IaaS.git
--- core.ps1
+++ core.ps1
@@ -1,2 +1,4 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force;
-iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'));
+iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/christian-dcruz/IaaS-Core/master/chocolatey.ps1'));
+
