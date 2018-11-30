fa5d7ef245712aaaacf5dc3fa2145d362f3c75bc
Christian <christian@dcruz.it>
2018-11-30 22:01:40 +0000

    and again

--- core.ps1
+++ core.ps1
@@ -1,6 +1,5 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force;
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'));
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine");
-choco install sysinternals;
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/christian-dcruz/IaaS-Core/master/chocolatey.ps1'));
 
