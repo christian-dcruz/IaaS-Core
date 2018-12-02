0e334d324e8d1367fbfde433f301df65740ca6bb
Christian <christian@dcruz.it>
2018-12-02 21:21:01 +0000

    Added vsExtensions.ps1 to core.ps1

--- core.ps1
+++ core.ps1
@@ -2,4 +2,6 @@ Set-ExecutionPolicy Bypass -Scope Process -Force;
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'));
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine");
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/christian-dcruz/IaaS-Core/master/chocolatey.ps1'));
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine");
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/christian-dcruz/IaaS-Core/master/vsExtensions.ps1'));
 
