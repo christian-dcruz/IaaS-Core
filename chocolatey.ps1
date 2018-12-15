8ffcf3b8fbd779f5334bdb0703a049079ef6244e
Christian <christian@dcruz.it>
2018-12-15 12:39:50 +0000

    Uncommented a few more choco packages and added docker

--- chocolatey.ps1
+++ chocolatey.ps1
@@ -1,15 +1,16 @@
-# choco install -y chocolatey-core.extension
+choco install -y chocolatey-core.extension
 choco install -y googlechrome
 choco install -y git
 choco install -y tortoisegit
-# choco install -y dotnetcore-sdk
-# choco install -y nodejs
+choco install -y dotnetcore-sdk
+choco install -y nodejs
 choco install -y vscode
-# choco install -y visualstudio2017community
+choco install -y visualstudio2017community
 choco install -y gnuwin32-coreutils.install
 choco install -y curl
-# choco install -y sysinternals
-# choco install -y notepadplusplus
+choco install -y sysinternals
+choco install -y notepadplusplus
 choco install -y treesizefree
 choco install -y dropbox
 choco install -y keepass
+choco install -y docker
